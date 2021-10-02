5111f43c7796a9be39f16d02f1d9441bff60ea73
Matthew Thomson <mthomson.lee@gmail.com>
2021-10-01 23:28:35 -0400

    EX10 drills

--- ex10.rb
+++ ex10.rb
@@ -2,12 +2,13 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslack_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
-I'll do a list:
+fat_cat = '''
+I\'ll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+also #{backslack_cat}
+'''
 
 puts tabby_cat
 puts persian_cat
