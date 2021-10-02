ae9af13f13c665b98d281cc56d659759bfba0ce0
Matthew Thomson <mthomson.lee@gmail.com>
2021-10-01 22:56:25 -0400

    EX6 drills

--- ex-06/ex6.rb
+++ ex-06/ex6.rb
@@ -1,14 +1,14 @@
 types_of_people = 10
 x = "There are #{types_of_people} types of people."
-binary = "binary"
+binary = 'binary'
 do_not = "don't"
-y = "Those who know #{binary} and those who #{do_not}"
+y = 'Those who know #{binary} and those who #{do_not}'
 
 
 puts x
 puts y
 
-puts "I said: #{x}"
+puts 'I said: #{x}'
 puts "I also said: '#{y}'"
 
 hilarious = false
