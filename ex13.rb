c3377a1d1b2a8ff4c637875affc504b12c87fffa
Matthew Thomson <mthomson.lee@gmail.com>
2021-10-01 23:46:13 -0400

    EX13 done

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+first, second, third = ARGV
+
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+
+print "Next var is "
+fourth = $stdin.gets.chomp
+
+puts "Your fourth variable is #{fourth}"
