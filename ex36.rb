4d17e0fe49aeecd9befbb771d623c6c27bec5c3b
Matthew Thomson <mthomson.lee@gmail.com>
2021-10-04 15:16:01 -0400

    EX36 finishes the game

--- ex36.rb
+++ ex36.rb
@@ -24,6 +24,10 @@ module StandardResponse
   def StandardResponse.bad_end
     puts "\n\nYou lose."
   end
+  
+  def StandardResponse.good_end
+    puts "\n\nYou win!"
+  end
 end
 
 module Room
@@ -79,7 +83,7 @@ module MainHall
   def MainHall.move
     Room.look("to the north (N) you see #{ThroneRoom.far_description}")
     Room.look("to the east (E) you see #{Kitchen.far_description}")
-    Room.look("to the west (W) you see dark, stairs going down.")
+    Room.look("to the west (W) you see #{Dungeon.far_description}")
     move = Room.move(Valid_directions)
     case move
     when 'N'
@@ -87,6 +91,7 @@ module MainHall
     when 'E'
       Kitchen.enter(Room_name)
     when 'W'
+      Dungeon.enter(Room_name)
     end
   end
 
@@ -96,12 +101,20 @@ module MainHall
     action = Room.action(['S', 'I'])
     case action
     when 'S'
-      puts 'You sit down in the surprisingly comforatable chair. Your body relaxes in its cloud-like embrace. You comforatably drift into your final sleep.'
-      StandardResponse.bad_end
+      MainHall.comfy_chair
     when 'I'
-      puts 'You walk over to the long crack and examine it. Curious if it is stable, you knock it with your hand. Dust shakes from the ceiling. Creeking strain of stone echos through the hall. You lean against the pill and slide to the ground, accepting the foreseen outcome.'
-      StandardResponse.bad_end
+      MainHall.room_collapse
     end
+  end
+
+  def MainHall.comfy_chair
+    puts 'You sit down in the surprisingly comforatable chair. Your body relaxes in its cloud-like embrace. You comforatably drift into your final sleep.'
+    StandardResponse.bad_end
+  end
+
+  def MainHall.room_collapse
+    puts 'You walk over to the long crack and examine it. Curious if it is stable, you knock it with your hand. Dust shakes from the ceiling. Creeking strain of stone echos through the hall. You lean against the pill and slide to the ground, accepting the foreseen outcome.'
+    StandardResponse.bad_end
   end
 end
 
@@ -131,11 +144,12 @@ module ThroneRoom
   end
 
   def ThroneRoom.move
-    Room.look('to the northeast (NE) you see a flickering candle in a small passageway')
+    Room.look("to the northeast (NE) you see a flickering candle in #{ButlerHallway.far_description}")
     Room.look("to the south (S) you see #{MainHall.far_description}")
     move = Room.move(Valid_directions)
     case move
     when 'NE'
+      ThroneRoom.armor_death('small passageway')
     when 'S'
       MainHall.enter(Room_name)
     end
@@ -147,12 +161,25 @@ module ThroneRoom
     action = Room.action(['A', 'T'])
     case action
     when 'A'
-      puts 'The dead king\'s eyes light a blaze as you approach. Fog pours from his loose jaw. A deep rumbling stops as abruptly as it started. Without ceremony, your existance is snuffed out.'
-      StandardResponse.bad_end
+      ThroneRoom.kings_penalty
     when 'T'
-      puts 'WIth great effort you rotate the armor stand to face forward. As you turn around, the armor creeks. You prepare for the worst, but as you turn around you see it rotate around again.'
-      ThroneRoom.decide
+      ThroneRoom.move_armor
     end
+  end
+
+  def ThroneRoom.kings_penalty
+    puts 'The dead king\'s eyes light a blaze as you approach. Fog pours from his loose jaw. A deep rumbling stops as abruptly as it started. Without ceremony, your existance is snuffed out.'
+    StandardResponse.bad_end
+  end
+
+  def ThroneRoom.armor_death(walk_location)
+    puts "You walk towards the #{walk_location}, but are met by an oversided suit of armor. You take one fatal step too close. The armor cuts you down in one swift motion."
+    StandardResponse.bad_end
+  end
+
+  def ThroneRoom.move_armor
+    puts 'WIth great effort you rotate the armor stand to face forward. As you turn around, the armor creeks. You prepare for the worst, but as you turn around you see it rotate around again.'
+    ThroneRoom.decide
   end
 end
 
@@ -182,11 +209,12 @@ module Kitchen
   end
 
   def Kitchen.move
-    Room.look("to the north (N) you see a small butler hallway")
+    Room.look("to the north (N) you see #{ButlerHallway.far_description}")
     Room.look("to the west (W) you see #{MainHall.far_description}")
     move = Room.move(Valid_directions)
     case move
     when 'N'
+      ButlerHallway.enter(Room_name)
     when 'W'
       MainHall.enter(Room_name)
     end
@@ -198,12 +226,67 @@ module Kitchen
     action = Room.action(['T', 'A'])
     case action
     when 'T'
-      puts 'You sneakily grab the haunch of meat and take a bite. An intense wave of gluttony overcomes you. You stand there and eat and eat and eat. You notice the undead chef keeps the meat coming. It\'s your last thought.'
-      StandardResponse.bad_end
+      Kitchen.too_much_food
     when 'A'
-      puts 'You grab the cleaver, yell, and charge at the Chef. Swinging wildly with your eyes shut. You connect! You strike again and again. You open eyes to bask in your victory, but are met with the chef\'s face of sadness. It sulks and mumbles, "I just want to cook good food."'
-      puts 'A bit baffled you feel bad and put down the cleaver.'
-      Kitchen.decide
+      Kitchen.poor_chef
+    end
+  end
+
+  def Kitchen.too_much_food
+    puts 'You sneakily grab the haunch of meat and take a bite. An intense wave of gluttony overcomes you. You stand there and eat and eat and eat. You notice the undead chef keeps the meat coming. It\'s your last thought.'
+    StandardResponse.bad_end
+  end
+
+  def Kitchen.poor_chef
+    puts 'You grab the cleaver, yell, and charge at the Chef. Swinging wildly with your eyes shut. You connect! You strike again and again. You open eyes to bask in your victory, but are met with the chef\'s face of sadness. It sulks and mumbles, "I just want to cook good food."'
+    puts 'A bit baffled you feel bad and put down the cleaver.'
+    Kitchen.decide
+  end
+end
+
+module Dungeon
+  Room_name = 'Dungeon'
+  Description = 'a thick fog as you descend the spiral staircase. The torches become further apart until there is barely any light. Holding your hand against the wall you continue to descend. Your next and final step is no step at all. The stairs have ended. You fall into the void never to be see again.'
+
+  def Dungeon.far_description
+    return 'dark, stairs going down.'
+  end
+
+  def Dungeon.enter(previous_room)
+    Room.enter(Room_name, previous_room)
+    Room.look(Description)
+    StandardResponse.bad_end
+  end
+end
+
+module ButlerHallway
+  Room_name = 'Butler Hallway'
+  Valid_directions = ['N', 'S', 'W']
+  Description = 'a small hallway with a faint light towards the end.'
+
+  def ButlerHallway.far_description
+    return 'a small butler hallway'
+  end
+
+  def ButlerHallway.enter(previous_room)
+    Room.enter(Room_name, previous_room)
+    Room.look(Description)
+    ButlerHallway.move
+  end
+
+  def ButlerHallway.move
+    Room.look("to the north (N) you see a faint light.")
+    Room.look("to the south (S) you see #{Kitchen.far_description}")
+    Room.look("to the west (W) you see #{ThroneRoom.far_description}")
+    move = Room.move(Valid_directions)
+    case move
+    when 'N'
+      puts 'You feel a breeze of fresh air. You can sense the exit is just ahead.'
+      StandardResponse.good_end
+    when 'S'
+      Kitchen.enter(Room_name)
+    when 'W'
+      ThroneRoom.armor_death('Throne Room')
     end
   end
 end
