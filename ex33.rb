4fe73f494a5068dec7c592fbdada240b6aa0b970
Matthew Thomson <mthomson.lee@gmail.com>
2021-10-03 23:56:35 -0400

    EX33 drills - ranges are sweet

--- ex33.rb
+++ ex33.rb
@@ -1,14 +1,35 @@
-i = 0
-numbers = []
+def count_by(max, inc)
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
+  while i < max
+    #puts "At the top i is #{i}"
+    numbers.push(i)
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += inc
+    #puts "Numbers now: ", numbers
+    #puts "At the bottom i is #{i}"
+  end
+
+  puts "The numbers (while): "
+  numbers.each {|num| puts num }
 end
 
-puts "The numbers: "
-numbers.each {|num| puts num }
+def count_for(max, inc) 
+  numbers = []
+  (0 ... max).step(inc).each {|i|
+    #puts "At the top i is #{i}"
+    numbers.push(i)
+
+    #puts "Numbers now: ", numbers
+    #puts "At the bottom i is #{i}"
+
+  }
+  puts "The numbers (for): "
+  numbers.each {|num| puts num }
+end
+
+count_by(6, 2)
+count_by(2, 1)
+count_for(6, 2)
+count_for(2, 1)
